fcbcccdd3e8d33dc2b0fe0b9c71c089a198c0a42
Duarte <guilhermeduartt21@gmail.com>
2021-06-03 14:58:20 -0300

    remove test

--- src/database/index.test.js
+++
@@ -1,7 +0,0 @@
-const index = require("./index");
-const { test, expect } = require("@jest/globals");
-
-test("Connecting Database", async () => {
-  const connection = index();
-  expect(connection).toBeTruthy();
-});
